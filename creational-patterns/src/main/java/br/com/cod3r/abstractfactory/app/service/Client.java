f5fd6d188ec7adbc0da8842263f5c243d16d2d7a
Diego Rodrigues de Sousa <diegors90@gmail.com>
2022-04-23 19:13:03 -0300

    add exemple two abstract factory

--- creational-patterns/src/main/java/br/com/cod3r/abstractfactory/app/service/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/abstractfactory/app/service/Client.java
@@ -1,8 +1,26 @@
 package br.com.cod3r.abstractfactory.app.service;
 
+import br.com.cod3r.abstractfactory.app.service.factory.EJBAbstractFactory;
+import br.com.cod3r.abstractfactory.app.service.factory.RestAbstractFactory;
+import br.com.cod3r.abstractfactory.app.service.factory.ServicesAbstractFactory;
+import br.com.cod3r.abstractfactory.app.service.services.CarService;
+import br.com.cod3r.abstractfactory.app.service.services.UserService;
+
 public class Client {
 
 	public static void main(String[] args) {
+		ServicesAbstractFactory ejbFactory = new EJBAbstractFactory();
+		ServicesAbstractFactory restFactory = new RestAbstractFactory();
+
+		UserService userService = ejbFactory.getUserService();
+		userService.save("Jhon");
+		userService.delete(5);
+
+		CarService carService = restFactory.getCarService();
+		carService.save("Onix");
+		carService.update("Tesla");
+
+
 
 	}
 }
