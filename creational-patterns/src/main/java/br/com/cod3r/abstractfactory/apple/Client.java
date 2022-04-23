2abb5d5b40d7c3a1dc1c1269f40540b3c787c1e7
Diego Rodrigues de Sousa <diegors90@gmail.com>
2022-04-23 18:35:01 -0300

    add exemple one abstract factory

--- creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
@@ -1,8 +1,26 @@
 package br.com.cod3r.abstractfactory.apple;
 
+import br.com.cod3r.abstractfactory.apple.factory.IPhone11Factory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneXFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.BrazilianRuleAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.CountryRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.model.iphone.IPhone;
+
 public class Client {
 	
 	public static void main(String[] args) {
+		CountryRulesAbstractFactory brazilianRules = new BrazilianRuleAbstractFactory();
+		IPhoneXFactory genXFactory = new IPhoneXFactory(brazilianRules);
+		IPhone11Factory gen11Factory = new IPhone11Factory(brazilianRules);
+
+		System.out.println("### Ordering an IPhone X");
+		IPhone iphoneX = genXFactory.orderIPhone("standard");
+		System.out.println(iphoneX);
+
+		System.out.println("### Ordering an IPhone 11");
+		IPhone iPhone11 = gen11Factory.orderIPhone("highEnd");
+		System.out.println(iPhone11);
+
 
 	}
 }
