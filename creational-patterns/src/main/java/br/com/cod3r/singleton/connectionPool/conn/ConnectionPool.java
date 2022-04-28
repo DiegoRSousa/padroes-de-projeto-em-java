f09174ab92a5e7cec220decaaf6d5deafbdd7bb0
Diego Rodrigues de Sousa <diegors90@gmail.com>
2022-04-27 22:17:19 -0300

    add exemplo singleton

--- creational-patterns/src/main/java/br/com/cod3r/singleton/connectionPool/conn/ConnectionPool.java
+++ creational-patterns/src/main/java/br/com/cod3r/singleton/connectionPool/conn/ConnectionPool.java
@@ -6,8 +6,13 @@ import java.util.List;
 public class ConnectionPool {
 	private final static int POOL_SIZE = 2;
 	private List<Connection> connectionsPool;
+	private static ConnectionPool singleton = new ConnectionPool();
+
+	public static ConnectionPool getInstance() {
+		return singleton;
+	}
 	
-	public ConnectionPool() {
+	private ConnectionPool() {
 		System.out.println("Creating Connection Pool");
 		connectionsPool = new ArrayList<Connection>();
 		for(int i = 0; i < POOL_SIZE; i++) {
