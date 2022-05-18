0c2fcedc7feed6a94cdcb13ec93f121cf5eb5a24
Diego Rodrigues de Sousa <diegors90@gmail.com>
2022-05-18 07:54:57 -0300

    adiciona exemplo em conjunto com builder

--- creational-patterns/src/main/java/br/com/cod3r/prototype/functionalCloneBuilder/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/prototype/functionalCloneBuilder/Client.java
@@ -8,5 +8,8 @@ public class Client {
 	public static void main(String[] args) throws CloneNotSupportedException {
 		User user = new User("ToClone", 25, new Address("ABC Street", 1000));
 		System.out.println(user);
+
+		User cloneUser = user.cloneBuilder().withName("Clone++").now();
+		System.out.println(cloneUser);
 	}
 }
--- creational-patterns/src/main/java/br/com/cod3r/prototype/functionalCloneBuilder/model/User.java
+++ creational-patterns/src/main/java/br/com/cod3r/prototype/functionalCloneBuilder/model/User.java
@@ -1,6 +1,6 @@
 package br.com.cod3r.prototype.functionalCloneBuilder.model;
 
-public class User {
+public class User implements Cloneable {
 	private String name;
 	private Integer age;
 	private Address address;
@@ -27,5 +27,43 @@ public class User {
 	@Override
 	public String toString() {
 		return "User [name=" + name + ", age=" + age + ", address=" + address + "]";
+	}
+
+
+
+	public Builder cloneBuilder() {
+		return new Builder(name, age, address);
+	}
+
+
+	public static class Builder {
+		private String name;
+		private Integer age;
+		private Address address;
+
+		private Builder(String name, Integer age, Address address) {
+			this.name = name;
+			this.age = age;
+			this.address = address;
+		}
+
+		public Builder withName(String name) {
+			this.name = name;
+			return this;
+		}
+
+		public Builder withAge(Integer age) {
+			this.age = age;
+			return this;
+		}
+
+		public Builder withAddress(Address address) {
+			this.address = address;
+			return this;
+		}
+
+		public User now() {
+			return new User(name, age, address);
+		}
 	}
 }
